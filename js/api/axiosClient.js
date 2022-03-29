0468a732015b791b767088077831da2bb41fe0d3
data61999 <datvna61999@gmail.com>
2022-03-29 10:45:07 +0700

    setup default axios

---
+++ js/api/axiosClient.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+  baseURL: 'https://js-post-api.herokuapp.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosClient.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+export default axiosClient;
